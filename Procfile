41ea80ce2ec65e9f182601e40961aae0db793b8c
trant <trant@icts.vn>
2017-09-22 17:22:01 +0700

    update code

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn checkin.wsgi
+web: gunicorn checkin.wsgi --log-file -
--- heroku-django/settings.py
+++ heroku-django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'x*-qjiag512br3cuc#)liq*yj-dwv@&#ys_y9fi2*p!+hcl^cb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
